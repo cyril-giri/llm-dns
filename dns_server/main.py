c971e46985fa8eae208ff5a7c96724595b303fb9
Cyril Stafford Giri <cyrilgiri001@gmail.com>
2025-08-26 01:59:01 +0530

    udp server setup

--- dns_server/main.py
+++ dns_server/main.py
@@ -1,7 +1,21 @@
 import socket
 
+DNS_PORT = 53
+
 def main():
-    pass
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+        s.bind(("", DNS_PORT))
+        print(f"DNS server listening on port {DNS_PORT}")
+
+        try:
+            while True:
+                data, addr = s.recvfrom(512)  # DNS messages are typically up to 512 bytes
+                print(f"Received DNS query from {addr}")
+        except KeyboardInterrupt:
+                print("Shutting down DNS server.")
+        finally:
+                s.close()
+
 
 if __name__ == "__main__":
     main()
